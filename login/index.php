eef015c3aead4af372c4fe2b72582367ba9f1425
Abdellah El Habchi <aelhabchi@rightplacecall.local>
2024-01-23 15:25:56 +0100

    add login page

--- login/index.php
+++ login/index.php
@@ -37,7 +37,6 @@ ini_set('display_errors', 'On');
 </head>
 
 <body>
-
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100 p-b-160 p-t-50">
@@ -59,24 +58,19 @@ ini_set('display_errors', 'On');
 					<span class="login100-form-title p-b-43">
 						<img src="..\images\logo.png" width="400"><br>
 					</span>
-
 					<div class="wrap-input100 rs1 validate-input" data-validate="Nom d'utilisateur est obligatoire">
 						<input class="input100" type="text" name="username">
 						<span class="label-input100">Nom d'utilisateur</span>
 					</div>
-
-
 					<div class="wrap-input100 rs2 validate-input" data-validate="Mot de passe est obligatoire">
 						<input class="input100" type="password" name="password">
 						<span class="label-input100">Mot de Passe</span>
 					</div>
-
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn" type="submit" name="login">
 							CONNEXION
 						</button>
 					</div>
-
 					<div class="text-center w-full p-t-23">
 						<a href="#" class="txt1">
 							© 2019 | StrictAssur - Boite Mail WEB
@@ -86,27 +80,30 @@ ini_set('display_errors', 'On');
 			</div>
 		</div>
 	</div>
+</body>
+
+</html>
 
 
 
 
 
-	<!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
-	<!--===============================================================================================-->
-	<script src="js/main.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/daterangepicker/moment.min.js"></script>
+<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+<script src="js/main.js"></script>
 
 
 </body>
